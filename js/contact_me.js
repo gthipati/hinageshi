79c98aab92517a2a690f234b38a5b0d66caecb32
Gen <32942952+gthipati@users.noreply.github.com>
2019-10-24 15:22:10 -0700

    Add files via upload

--- js/contact_me.js
+++ js/contact_me.js
@@ -10,7 +10,7 @@ $(function() {
       // get values from FORM
       var name = $("input#name").val();
       var email = $("input#email").val();
-      var subject = $("input#subject").val();
+      var phone = $("input#phone").val();
       var message = $("textarea#message").val();
       var firstName = name; // For Success/Failure Message
       // Check for white space in name for Success/Fail message
@@ -24,7 +24,7 @@ $(function() {
         type: "POST",
         data: {
           name: name,
-          subject: subject,
+          phone: phone,
           email: email,
           message: message
         },
@@ -46,7 +46,7 @@ $(function() {
           $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please e-mail me at admin@genthipa.com!"));
+          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
           $('#success > .alert-danger').append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
